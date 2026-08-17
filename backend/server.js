8c2bea42def20868786f5b27999545027fa9e8f7
theirhasnain <theirhasnain@gmail.com>
2026-08-17 05:52:28 -0700

    edited

--- backend/server.js
+++ backend/server.js
@@ -34,5 +34,5 @@ app.get("/", (req, res) => {
   res.send("Watch store backend start")
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT= process.env.CLIENT_URL || 5000;
 app.listen(PORT, () => console.log(`Server chal raha hai: http://localhost:${PORT}`));
